944e683eb31c2008698765c630951ed4c02b13be
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2025-03-04 16:57:40 -0300

    feature - finishes the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="h-16 border">
+<div class="h-16">
 	<div class="flex place-content-between p-6">
 		<div>
 			<span>Bárbara Port</span>
@@ -9,12 +9,31 @@
 		</div>
 	</div>
 </div>
-
-<div class="h-[calc(100vh-64px)] border">
-    <div class="h-full flex flex-col items-center justify-center">
-        <h2>Looking for an expert software developer? Let’s work together to find your solution.</h2>
-        <h1>Building the future, one line of code at a time.</h1>
-    </div>
+<div class="bg h-[calc(100vh-64px)] bg-cover bg-center bg-no-repeat">
+	<div class="flex h-full flex-col items-center justify-center">
+		<div class="flex h-1/2 flex-col items-center justify-end gap-2.5">
+			<span class="text-xl"
+				>Looking for an expert software developer? Let’s work together to find your solution.</span
+			>
+			<span class="text-5xl font-medium">Building the future, one line of code at a time.</span>
+			<span class="text-sm"
+				>Photo by <a
+					href="https://unsplash.com/@mitchel3uo?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+					>Mitchell Luo</a
+				>
+				on
+				<a
+					href="https://unsplash.com/photos/black-and-white-striped-textile-FWoq_ldWlNQ?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+					>Unsplash</a
+				>
+			</span>
+		</div>
+		<div class="flex h-1/2 flex-col items-center justify-end">
+			<div class="mb-5">
+				<img src="/src/assets/icons/arrow.png" alt="arrow down" />
+			</div>
+		</div>
+	</div>
 </div>
 
 <div class="h-dvh border">projects</div>

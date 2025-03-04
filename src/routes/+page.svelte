09b3592fbc2eeb45e67faeb5d87043d1da675b01
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2025-03-03 22:16:17 -0300

    feature - adds skeleton, basic header and basic main div

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to Barbara's Portfolio</h1>
-<p>Hello World!</p>
+<div class="h-16 border">
+	<div class="flex place-content-between p-6">
+		<div>
+			<span>Bárbara Port</span>
+		</div>
+		<div class="flex gap-7">
+			<a href="/projects">Projects</a>
+			<a href="/about-me">About me</a>
+		</div>
+	</div>
+</div>
+
+<div class="h-[calc(100vh-64px)] border">
+    <div class="h-full flex flex-col items-center justify-center">
+        <h2>Looking for an expert software developer? Let’s work together to find your solution.</h2>
+        <h1>Building the future, one line of code at a time.</h1>
+    </div>
+</div>
+
+<div class="h-dvh border">projects</div>
+
+<div class="h-dvh border">about me</div>
+
+<div class="h-24 border">footer</div>

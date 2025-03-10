5e8f474b666fc1af95d2a33b6914c0744d8db852
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2025-03-09 22:14:43 -0300

    feature: adds about me and footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Project from '$lib/components/project.svelte';
+	import Skill from '$lib/components/skill.svelte';
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
@@ -65,6 +66,144 @@
 	</div>
 </div>
 
-<div class="h-dvh border">about me</div>
+<div class="h-[calc(100vh-96px)]">
+	<div class="flex flex-col items-center justify-center gap-3">
+		<div class="mx-6 flex flex-col gap-6 py-3">
+			<div class="flex flex-col items-center justify-center gap-3">
+				<span class="text-4xl font-semibold">About me</span>
+				<div class="text-2xl">
+					<span>Nice to meet you. I'm </span>
+					<span class="font-bold">Bárbara Port</span><span>.</span>
+				</div>
+			</div>
+			<div class="flex w-full content-between gap-3">
+				<div class="flex w-full items-center justify-center">
+					<img
+						src="/src/assets/me.png"
+						aria-hidden="true"
+						class="rounded-full"
+						width="256"
+						alt="Barbara's profile picture. The picture has a black background. She is a woman with a black t-shirt, that has curly and short hair. Also, she uses glasses."
+					/>
+				</div>
+				<div class="w-full">
+					<div class="mb-4"><span class="text-xl">Overview</span></div>
+					<ul role="list" class="marker:--color-text ml-6 list-disc">
+						<li>I have 3 years of professional experience.</li>
+						<li>I have been studying software development for 8 years.</li>
+					</ul>
 
-<div class="h-24 border">footer</div>
+					<div class="my-4">
+						<span class="text-xl">Top Skills</span>
+					</div>
+					<div class="flex flex-wrap gap-3">
+						<Skill name="Lorem" />
+						<Skill name="Lorem" />
+						<Skill name="Lorem" />
+						<Skill name="Lorem" />
+						<Skill name="Lorem" />
+						<Skill name="Lorem" />
+					</div>
+				</div>
+				<div class="w-full">
+					<div class="mb-4"><span class="text-xl">Work</span></div>
+					<div class="flex flex-col gap-4">
+						<div>
+							<div class="flex justify-between">
+								<span>Company ABC</span>
+								<span>(xxxx - xxxx)</span>
+							</div>
+							<ul role="list" class="marker:--color-text ml-6 list-disc">
+								<li>A really big description to see if the word wrapping is correct</li>
+							</ul>
+						</div>
+						<div>
+							<div class="flex justify-between">
+								<span>Company DEF</span>
+								<span>(xxxx - xxxx)</span>
+							</div>
+							<ul role="list" class="marker:--color-text ml-6 list-disc">
+								<li>A really big description to see if the word wrapping is correct</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="w-full">
+					<div class="mb-4">
+						<span class="text-xl">Education</span>
+					</div>
+					<div class="flex flex-col gap-4">
+						<div>
+							<div class="flex justify-between">
+								<span>University ABC</span>
+								<span>(xxxx - xxxx)</span>
+							</div>
+							<ul role="list" class="marker:--color-text ml-6 list-disc">
+								<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+							</ul>
+						</div>
+						<div>
+							<div class="flex justify-between">
+								<span>School DEF</span>
+								<span>(xxxx - xxxx)</span>
+							</div>
+							<ul role="list" class="marker:--color-text ml-6 list-disc">
+								<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="flex w-full items-center justify-center text-nowrap">
+			<a href="https://linkedin.com/in/barbaraport" class="flex gap-3 text-4xl font-bold"
+				>Let's work together
+				<div class="flex items-center justify-center">
+					<img
+						width="24"
+						src="/src/assets/icons/external-link.png"
+						alt="external link to my LinkedIn profile."
+					/>
+				</div>
+			</a>
+		</div>
+	</div>
+</div>
+
+<div class="h-24">
+	<hr class="border-divider" />
+	<div class="flex h-full w-full items-center justify-end p-6">
+		<div class="grid h-full w-full grid-cols-3 items-center justify-between">
+			<div class="col-start-2 flex flex-col items-center justify-center">
+				<span>Bárbara Port © {`${new Date().getFullYear()}`}</span>
+				<div class="text-xs">
+					<span>Made with </span>
+					<span class="font-medium">Svelte</span>
+					<span>and </span>
+					<span class="font-medium">Tailwind CSS</span>
+					<span>{'<3'}</span>
+				</div>
+			</div>
+			<div class="col-start-3 flex items-center justify-end gap-6">
+				<a href="https://linkedin.com/in/barbaraport"
+					><img
+						src="/src/assets/icons/linkedin.png"
+						alt="icon that when clicked, redirects to my LinkedIn profile."
+					/></a
+				>
+				<a href="https://github.com/barbaraport"
+					><img
+						src="/src/assets/icons/github.png"
+						alt="icon that when clicked, redirects to my Github profile."
+					/></a
+				>
+				<a href="https://www.youtube.com/"
+					><img
+						src="/src/assets/icons/youtube.png"
+						alt="icon that when clicked, redirects to my YouTube channel."
+					/></a
+				>
+			</div>
+		</div>
+	</div>
+</div>

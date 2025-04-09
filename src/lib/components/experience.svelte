505fd951a7615790be175a4c0bbfe357e2c1df88
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2025-04-09 13:37:45 -0300

    refactor - improves experience component, creating a method to format the start and end dates

--- src/lib/components/experience.svelte
+++ src/lib/components/experience.svelte
@@ -2,17 +2,23 @@
 	import type { ExperienceProps } from './dtos/ExperienceProps';
 
 	const { experience }: ExperienceProps = $props();
+
+	function getYearFromDate(date?: Date | string): number {
+		if (!date) return new Date().getFullYear();
+
+		return new Date(date).getFullYear();
+	}
 </script>
 
 <div>
 	<div class="flex justify-between">
-		<div class="w-full"><span>{experience.name}</span></div>
+		<div>
+			<div class="w-full"><span>{experience.name}</span></div>
+			<div class="w-full"><span class="text-sm">{experience.place}</span></div>
+		</div>
+
 		<div class="nowrap flex justify-end whitespace-nowrap">
-			<span
-				>({new Date(experience.startDate).getFullYear()} - {new Date(
-					experience.endDate
-				).getFullYear()})</span
-			>
+			<span>({getYearFromDate(experience.startDate)} - {getYearFromDate(experience.endDate)})</span>
 		</div>
 	</div>
 	<ul role="list" class="marker:--color-text ml-6 list-disc">

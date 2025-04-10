c9e00c11cc27f9bcb9b5cc03aeda8d3f9df4666a
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2025-04-10 19:28:51 -0300

    fix - fixes main section responsivity problems, to ensure a great user experience

--- src/lib/sections/main/mainSection.svelte
+++ src/lib/sections/main/mainSection.svelte
@@ -5,14 +5,17 @@
 </script>
 
 <Navbar {info} />
-<div class="bg h-[calc(100vh-64px)] bg-cover bg-center bg-no-repeat">
+<div class="bg h-[calc(100vh-64px)] bg-cover bg-center bg-no-repeat px-4">
 	<div class="flex h-full flex-col items-center justify-center">
-		<div class="flex h-1/2 flex-col items-center justify-end gap-2.5">
-			<span class="text-xl"
+		<div class="center flex h-1/2 flex-col items-center justify-end gap-2.5 text-center">
+			<span class="max-[644px]:text-xs sm:text-sm md:text-sm lg:text-lg xl:text-xl 2xl:text-2xl"
 				>Looking for an expert software developer? Let's work together to find your solution.</span
 			>
-			<span class="text-5xl font-medium">Building the future, one line of code at a time.</span>
-			<span class="text-sm"
+			<span
+				class="font-medium max-[644px]:text-lg sm:text-lg md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl"
+				>Building the future, one line of code at a time.</span
+			>
+			<span class="text-xs"
 				>Photo by <a
 					href="https://unsplash.com/@mitchel3uo?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
 					>Mitchell Luo</a

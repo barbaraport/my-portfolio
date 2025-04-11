cc4f1e5efcce02d2e774b356f47e4a4aaa3c1204
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2025-04-10 21:09:31 -0300

    fix - finishes fixing responsivity issues on projects section, to improve user experience

--- src/lib/components/project/project.svelte
+++ src/lib/components/project/project.svelte
@@ -8,16 +8,16 @@
 	class="md::flex-col flex w-full rounded-lg border max-[644px]:flex-col sm:flex-col md:flex-col lg:flex-row"
 >
 	<div class="w-full lg:w-1/2">
-		<img src={image?.url} alt={image?.alt} class="w-full rounded-tl-lg rounded-bl-lg" />
+		<img src={image?.url} alt={image?.alt} class="w-full rounded-lg" />
 	</div>
-	<div class="m-6 flex w-full flex-col justify-between gap-2 lg:w-1/2">
+	<div class="flex w-full flex-col justify-between gap-3 p-6 lg:w-1/2">
 		<div>
 			<span class="text-xl font-bold">{name}</span>
 			<ul role="list" class="marker:--color-text ml-6 list-disc">
 				<li>{description}</li>
 			</ul>
 		</div>
-		<div class="flex flex-wrap gap-3">
+		<div class="mt-6 flex flex-wrap gap-3">
 			{#each skills as skill}
 				<Skill name={skill?.name} />
 			{/each}
